0a457af8b46dfe1f9baebbe7df49700934d1f01c
Pedro Veras <veraspedro1608@gmail.com>
2024-12-31 09:53:40 -0300

    feat: ✨ exception handler controller

---
+++ src/main/java/br/com/pedroveras/courses_api/exceptions/ErrorMessageDTO.java
@@ -0,0 +1,11 @@
+package br.com.pedroveras.courses_api.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class ErrorMessageDTO {
+    private String message;
+    private String field;
+}
