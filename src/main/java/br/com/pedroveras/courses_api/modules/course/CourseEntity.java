a3efd7e950b1b2894645af20ad5627dbf4a63bde
Pedro Veras <veraspedro1608@gmail.com>
2024-12-31 14:27:39 -0300

    feat: ✨ course table

--- src/main/java/br/com/pedroveras/courses_api/modules/course/CourseEntity.java
+++ src/main/java/br/com/pedroveras/courses_api/modules/course/CourseEntity.java
@@ -1,15 +1,24 @@
 package br.com.pedroveras.courses_api.modules.course;
 
+import java.time.LocalDateTime;
 import java.util.UUID;
 
+import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.validator.constraints.Length;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
 
 import lombok.Data;
 
 @Data
+@Entity(name = "course")
 public class CourseEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
     
     @NotBlank(message = "O nome é obrigatório")
@@ -20,6 +29,9 @@ public class CourseEntity {
     private String description;
 
     private String category;
-    
+
     private CourseStatus active;
+
+    @CreationTimestamp
+    private LocalDateTime createdAt;
 }
